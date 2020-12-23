b4dfcc4e80ee4a43f16e1c931cdf5e1b67927d08
Hasan Iqbal <hasaniqbal3393@gmail.com>
2020-12-23 21:22:29 +0500

    Fix

--- connection/db.js
+++ connection/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const URI = 'mongodb+srv://snagIt:snagitdb@cluster0.0ty7k.mongodb.net/forheroku?retryWrites=true&w=majority';
 
 const connectDB = async () => {
-  await mongoose.connect(URI,{useUnifiedTopology:true,useNewUrlParser:true, useFindAndModify: false})
+  await mongoose.connect(URI,{useUnifiedTopology:false,useNewUrlParser:true, useFindAndModify: false})
   console.log("connected to atlas!!!!")
 }
 
