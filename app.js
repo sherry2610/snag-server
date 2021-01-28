2f2be23d7444a0d5d49cde18e0b4529422f359e4
sherry2610 <shaharyarkhalid2610@gmail.com>
2021-01-28 05:51:11 +0500

    feat: paypal integration

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var fs = require('fs');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -37,6 +38,107 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+const paypal = require("paypal-rest-sdk");
+
+paypal.configure({
+  mode: "sandbox", //sandbox or live
+  client_id:
+      "ARLqN-Az987NFctLIRCTab5Ab56_S8y-OEY5zKInthRJqUo1eBQzk5DM9GIV0g5MQobFAYnVm8tsK3ej",
+  client_secret:
+      "EAQ_8nj47QjND2XimIe9AFasedX03qs0aKcnZcb4ojr4m5F9nIe-rt9e1aMtsaizSgzrfnSx7Jhr7-sd"
+});
+
+
+app.get('/paypal', async(req, res, next) => {
+  var html = fs.readFileSync('./html/paypal.html', 'utf8')
+  res.statusCode = 200;
+  res.send(html);
+})
+
+app.get("/paypal/pay", (req, res) => {
+  var create_payment_json = {
+      intent: "sale",
+      payer: {
+          payment_method: "paypal"
+      },
+      redirect_urls: {
+          return_url: "http://localhost:3000/success",
+          cancel_url: "http://localhost:3000/cancel"
+      },
+      transactions: [
+          {
+              item_list: {
+                  items: [
+                      {
+                          name: "item",
+                          sku: "item",
+                          price: "1.00",
+                          currency: "USD",
+                          quantity: 1
+                      }
+                  ]
+              },
+              amount: {
+                  currency: "USD",
+                  total: "1.00"
+              },
+              description: "This is the payment description."
+          }
+      ]
+  };
+
+  paypal.payment.create(create_payment_json, function(error, payment) {
+      if (error) {
+          throw error;
+      } else {
+          console.log("Create Payment Response");
+          console.log(payment);
+          res.redirect(payment.links[1].href);
+      }
+  });
+});
+
+app.get("/success", (req, res) => {
+  // res.send("Success");
+  var PayerID = req.query.PayerID;
+  var paymentId = req.query.paymentId;
+  var execute_payment_json = {
+      payer_id: PayerID,
+      transactions: [
+          {
+              amount: {
+                  currency: "USD",
+                  total: "1.00"
+              }
+          }
+      ]
+  };
+
+  paypal.payment.execute(paymentId, execute_payment_json, function(
+      error,
+      payment
+  ) {
+      if (error) {
+          console.log(error.response);
+          throw error;
+      } else {
+          console.log("Get Payment Response");
+          console.log(JSON.stringify(payment));
+          var html = fs.readFileSync('./html/success.html', 'utf8')
+          res.statusCode = 200;
+          res.send(html);
+          // res.render("success");
+      }
+  });
+});
+
+app.get("cancel", (req, res) => {
+  var html = fs.readFileSync('./html/cancel.html', 'utf8')
+  res.statusCode = 200;
+  res.send(html);
+  // res.render("cancel");
+});
 
 app.use(logger('dev'));
 app.use(express.json());
