b943a63630e649ffafe463aaffc9e71d3af4c7f4
sherry2610 <shaharyarkhalid2610@gmail.com>
2021-08-31 00:17:15 +0500

    make it better

--- connection/db.js
+++ connection/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://snagIt:snagitdb@cluster0.0ty7k.mongodb.net/<dbname>?retryWrites=true&w=majority';
+// const URI = 'mongodb+srv://snagIt:snagitdb@cluster0.0ty7k.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://ahmed:ahmed123@cluster0.55mjw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 const connectDB = async () => {
   await mongoose.connect(URI,{useUnifiedTopology:false,useNewUrlParser:true, useFindAndModify: false})
